283829294da20b3189794dd363077ce73bffa541
Harsh Singh <harshsingh24@yahoo.com>
2021-02-24 20:27:21 -0800

    Replacing morning to prevent unfriendly word
    updating morning to dawn to remove "morning wood" possibility from the possible combination.

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -33,7 +33,7 @@ module Haikunator
         icy delicate quiet white cool spring winter patient
         twilight dawn crimson wispy weathered blue billowing
         broken cold damp falling frosty green long late lingering
-        bold little morning muddy old red rough still small
+        bold little dawn muddy old red rough still small
         sparkling thrumming shy wandering withered wild black
         young holy solitary fragrant aged snowy proud floral
         restless divine polished ancient purple lively nameless
