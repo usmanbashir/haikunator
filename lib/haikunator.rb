ea9a14383e94c541f1b7ed6b8fc9a7d80dbf35b8
Weihan Li <weihanli101@gmail.com>
2022-12-12 17:34:39 -0500

    remove converversial adjectives

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -30,11 +30,11 @@ module Haikunator
     def adjectives
       %w(
         autumn hidden bitter misty silent empty dry dark summer
-        icy delicate quiet white cool spring winter patient
+        icy delicate quiet cool spring winter patient
         twilight dawn crimson wispy weathered blue billowing
         broken cold damp falling frosty green long late lingering
         bold little morning muddy old red rough still small
-        sparkling thrumming shy wandering withered wild black
+        sparkling thrumming shy wandering withered wild
         young holy solitary fragrant aged snowy proud floral
         restless divine polished ancient purple lively nameless
         dazzling brave happy enchanting gentle glorious handsome
